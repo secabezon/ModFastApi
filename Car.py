1badb9f8af1dff946a1795e59ccd2aca9f0ab81a
sebastian Cabezon <secabezon21@gmail.com>
2024-09-28 21:37:41 -0300

    arreglo y limmpieza cod

--- Car.py
+++ Car.py
@@ -1,43 +1,6 @@
 from pydantic import BaseModel
 
 class Car(BaseModel):
-    # year:int
-    # running:float
-    # status:int
-    # motor_volume:float
-    # price:int
-    # model_hyundai:float
-    # model_kia:float
-    # model_mercedes_benz:float
-    # model_nissan: float
-    # model_toyota: float
-    # motor_type_diesel:float
-    # motor_type_gas:float
-    # motor_type_hybrid:float
-    # motor_type_petrol:float
-    # color_beige:  float
-    # color_black:  float
-    # color_blue:   float
-    # color_brown:  float
-    # color_cherry: float
-    # color_clove:  float
-    # color_golden: float
-    # color_gray:   float
-    # color_green:  float
-    # color_orange: float
-    # color_other:  float
-    # color_pink:   float
-    # color_purple: float
-    # color_red:    float
-    # color_silver: float
-    # color_skyblue:float
-    # color_white:  float
-    # type_Coupe:   float
-    # type_Universal:float
-    # type_hatchback:float
-    # type_pickup:  float
-    # type_sedan:   float
-    # type_suv:     float
     Id:int
     model:str
     year:int
